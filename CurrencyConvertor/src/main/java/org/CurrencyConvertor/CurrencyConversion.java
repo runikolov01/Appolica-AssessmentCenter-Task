f966d97dad14e23d23ddb2bdfd5167c82039bf8e
Rumen Nikolov <45116925+runikolov01@users.noreply.github.com>
2024-06-07 20:23:19 +0300

    the main method is created

---
+++ CurrencyConvertor/src/main/java/org/CurrencyConvertor/CurrencyConversion.java
@@ -0,0 +1,11 @@
+package org.CurrencyConvertor;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CurrencyConversion {
+
+    public static void main(String[] args) {
+
+    }
+}
