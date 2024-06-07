087279f3936de3fc85a8679b46c2524e1ebbdc60
Rumen Nikolov <45116925+runikolov01@users.noreply.github.com>
2024-06-07 22:04:55 +0300

    saveConversion() method is added

--- CurrencyConvertor/src/main/java/org/CurrencyConvertor/JsonService.java
+++ CurrencyConvertor/src/main/java/org/CurrencyConvertor/JsonService.java
@@ -1,9 +1,40 @@
 package org.CurrencyConvertor;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class JsonService {
 
     private static final String FILE_NAME = "conversions.json";
 
+    public static void saveConversion(String date, double amount, String baseCurrency, String targetCurrency, double convertedAmount) throws IOException {
+        JSONArray conversions;
 
+        Path pathFile = Paths.get(FILE_NAME);
+        if (Files.exists(pathFile)) {
+            String content = new String(Files.readAllBytes(pathFile));
+            conversions = new JSONArray(content);
+        } else {
+            conversions = new JSONArray();
+        }
+        
+        JSONObject conversion = new JSONObject();
+        conversion.put("date", date);
+        conversion.put("amount", amount);
+        conversion.put("base_currency", baseCurrency);
+        conversion.put("target_currency", targetCurrency);
+        conversion.put("converted_amount", convertedAmount);
+
+        conversions.put(conversion);
+
+        try (FileWriter file = new FileWriter(FILE_NAME)) {
+            file.write(conversions.toString(2));
+        }
+    }
 }
